b8dd4a7efb9e841ddd3dfc61313c09ede4404196
Aqsa Qur <aqsaqurayshi@gmail.com>
2025-01-09 07:34:26 +0500

    added changes

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -4,7 +4,8 @@ import Image from 'next/image';
 
 const HeroSection = () => {
   return (
-    <div className="relative overflow-hidden bg-black">
+    <div className="relative overflow-hidden bg-black text-white">
+      <Common title='Home' subtitle='Main'/>
       <div className="container mx-auto py-16 px-4 z-20 text-white">
         <div className="flex flex-col items-center justify-center">
           <h1 className="text-4xl font-bold mb-4">The Art of Speed Food Quality</h1>
@@ -12,7 +13,6 @@ const HeroSection = () => {
           <button className="bg-[#FF9F0D] text-white font-bold py-2 px-4 rounded">
             View Menu
           </button>
-
         </div>
       </div>
     </div>
